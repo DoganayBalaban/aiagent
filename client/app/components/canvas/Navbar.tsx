61c2e4577ee9d06b47d353344f3d0ded2d5f88c7
Doğanay Balaban <balabandoganay@gmail.com>
2025-06-29 01:37:23 +0300

    bildirim lb

--- client/app/components/canvas/Navbar.tsx
+++ client/app/components/canvas/Navbar.tsx
@@ -1,24 +1,36 @@
 import { ArrowLeft, Save, Settings } from "lucide-react";
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import { useSnackbar } from "notistack";
 
 const Navbar = () => {
+  const { enqueueSnackbar, } = useSnackbar()
   const navigate = useNavigate();
   const [title, setTitle] = useState("isimsiz dosya");
   const handleRouteBack = () => {
     navigate(-1);
   };
+
+  const handleBlur = () => {
+    if (title.trim() === "") {
+      setTitle("isimsiz dosya");
+    }
+    enqueueSnackbar("Dosya adı güncellendi", { variant: "success" });
+  };
+
   return (
     <header className="w-full h-16 bg-[#F5F5F5] shadow-lg shadow-[#616161]">
       <nav className="flex justify-between items-center p-4 bg-[#FFFFFF] text-[#616161] m-auto">
         <div>
           <ArrowLeft className="text-black cursor-pointer w-10 h-10 p-2 rounded-4xl hover:bg-[#6a6969] transition duration-500" onClick={handleRouteBack}/>
         </div>
-        <div>
+        <div className="">
+          
           <input
             type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
+            onBlur={handleBlur}
             placeholder="Dosya Adı"
             autoFocus
             required
--- client/app/root.tsx
+++ client/app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { SnackbarProvider } from "notistack";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -33,9 +34,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body className="h-full">
+        <SnackbarProvider>
         {children}
         <ScrollRestoration />
         <Scripts />
+        </SnackbarProvider>
       </body>
     </html>
   );
